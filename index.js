4aee403e13159c13f4c95b9b57b3934b387ce9e1
Miladkkr <159642796+Miladkkr@users.noreply.github.com>
2024-02-10 20:36:47 +0330

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["USSR","Afsharid","RezaKoni"];
 
 
 let currentIndex = 0;
